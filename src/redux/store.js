d741b4156a038f336ed51e240eb3f6d37521a67b
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-02-25 14:29:04 -0600

    composeWithDevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import reducer from './rootReducer';
+//npm i redux-devtools-extension
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(reducer, initialstate);
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
